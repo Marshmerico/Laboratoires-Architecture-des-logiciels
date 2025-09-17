b7381cc821aa2e63328d3f607a74484244a1d420
tomroyer <tomroyer151003@gmail.com>
2025-09-17 16:52:47 -0400

    premier

---
+++ Admin.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Admin{
+    int adminID;
+    String name;
+    String email;
+
+    public Admin(int adminID, String name, String email){
+        Sca
+    }
+
+
+
+}
